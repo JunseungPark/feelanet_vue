dde49c411dfe9a00b3f313cd1f90c9c030cd44ea
unknown <@LAPTOP-36L086OL.feelanet.com>
2022-04-06 17:17:47 +0900

    불필요한 파일 삭제

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,13 @@
 <template>
   <div id="app">
-    <Header/>
-
-    <Main/>
-
-    <Footer/>
   </div>
 </template>
 
 <script>
-//헤더
-import Header from './components/Header.vue'
-//풋터
-import Footer from './components/Footer.vue'
-import Main from './views/Main'
 
 export default {
   name: 'App',
   components: {
-    Main,
-    Header,
-    Footer,
   }
 }
 </script>
