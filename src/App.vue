712f6bd01191037ae9e4e59b3aaf0dee6e1e8b78
unknown <@LAPTOP-36L086OL.feelanet.com>
2022-04-06 15:57:02 +0900

    풋터 컴포넌트 추가

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,16 @@
     <Header/>
 
     <Main/>
+
+    <Footer/>
   </div>
 </template>
 
 <script>
 //헤더
 import Header from './components/Header.vue'
+//풋터
+import Footer from './components/Footer.vue'
 import Main from './views/Main'
 
 export default {
@@ -16,6 +20,7 @@ export default {
   components: {
     Main,
     Header,
+    Footer,
   }
 }
 </script>
