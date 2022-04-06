57c03a690a45849045cdf8a2a4ba80c3c139e079
unknown <@LAPTOP-36L086OL.feelanet.com>
2022-04-06 15:53:21 +0900

    헤더 컴포넌트 추가

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
-  <Main/>
+  <div id="app">
+    <Header/>
+
+    <Main/>
+  </div>
 </template>
 
 <script>
+//헤더
+import Header from './components/Header.vue'
 import Main from './views/Main'
 
 export default {
   name: 'App',
   components: {
-    Main
+    Main,
+    Header,
   }
 }
 </script>
