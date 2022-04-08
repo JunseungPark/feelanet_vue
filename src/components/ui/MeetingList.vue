6f1212d58716b3118b1c877c5249d404a10b6a03
JunseungPark <dpfwl8745@gmail.com>
2022-04-08 10:26:29 +0900

    미팅 스테이트 기능

--- src/components/ui/MeetingList.vue
+++ src/components/ui/MeetingList.vue
@@ -6,7 +6,7 @@
             <div class="abbr-wrap">
                 <span class="abbr" :style="{background: setColor(meeting.topic), color:'#fff'}">{{meeting.topic}}</span>
                 <a v-if="meeting.isBookmark" class="abbr-bookmark active"><i class="icon-fux-star-fill ft-orange ft-m"></i></a>
-                <a v-if="!meeting.isBookmark" class="abbr-bookmark"><i class="icon-fux-star ft-gray ft-m"></i></a>
+                <a v-else-if="!meeting.isBookmark" class="abbr-bookmark"><i class="icon-fux-star ft-gray ft-m"></i></a>
             </div>
             <div class="ellipsis flex-1 m-l-m">
                 <h6 class="ft-l m-b-s no-padding ellipsis">{{meeting.title}}</h6>
@@ -22,8 +22,9 @@
         <div v-if="!meeting.isSecret" class="p-all-m">
             <div class="dp-flex flex-between m-b-s">
                 <span class="dp-block ft-m ellipsis"><i class="icon-fux-calendar m-r-xs dp-inblock align-center" style="width:33px;"></i> {{meeting.start_date + " ~ " + meeting.end_date}}</span>
-                <b class="dp-block ft-s bg-skyblue bdr-all bdr-lightblue p-l-s p-r-s no-wrap">진행중</b>
-                <b class="dp-block ft-s bg-darkgray bdr-all bdr-darkgray p-l-s p-r-s no-wrap">완료</b>
+                <b v-if="meeting.state == 2" class="dp-block ft-s bg-darkgray bdr-all bdr-darkgray p-l-s p-r-s no-wrap">완료</b>
+                <b v-else-if="meeting.state == 1" class="dp-block ft-s bg-skyblue bdr-all bdr-lightblue p-l-s p-r-s no-wrap">진행중</b>
+                <b v-else></b>
             </div>
             <span class="dp-block ft-m ellipsis m-b-s"><i class="icon-fux-location m-r-xs dp-inblock align-center" style="width:33px;"></i> {{meeting.room}}</span>
             <span class="dp-block ft-m ellipsis m-b-s"><b class="ft-s bdr-all bdr-lightblue ft-darkblue p-l-s p-r-s m-r-xs">관리</b>{{meeting.manage}}</span>
